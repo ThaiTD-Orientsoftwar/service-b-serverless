c3982cda5a2d5f2d4d1ea0042eea583bbe8369ce
Thai Tran Duy <thai.tran@orientsoftware.com>
2021-05-24 18:20:34 +0700

    first commit

---
+++ handler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import AWS from 'aws-sdk';
+
+const sqs = new AWS.SQS();
+
+module.exports.sqsMessage = (event, context, callback) => {
+  const params = {
+    MessageBody: 'Hola',
+  };
+
+  // sqs.sendMessage(params, (err, data) => {
+  //   if (err) {
+  //     console.log('Error', err);
+  //   } else {
+  //     console.log(data);
+  //   }
+  // });
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(
+  //     {
+  //       message: 'Go Serverless v1.0! Your function executed successfully!',
+  //       input: event,
+  //     },
+  //     null,
+  //     2
+  //   ),
+  // };
+  console.log('Hello');
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
