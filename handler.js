66e9375b9971ea3f18eb8542f27630804685d0c1
Thai Tran Duy <thai.tran@orientsoftware.com>
2021-05-26 18:01:54 +0700

    update code

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import AWS from 'aws-sdk';
+const AWS = require('aws-sdk');
 
 const sqs = new AWS.SQS();
 
@@ -27,7 +27,7 @@ module.exports.sqsMessage = (event, context, callback) => {
   //     2
   //   ),
   // };
-  console.log('Hello');
+  console.log(event);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
